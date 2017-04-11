e4e89e8ab92b1d0581fa0d0eb7cb2b38a9674800
Milan Kovacs <kovacsmilan85@gmail.com>
2017-04-11 14:32:23 +0200

    add function to count letter A in file

--- countas/CountAs.java
+++ countas/CountAs.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CountAs {
   public static void main(String[] args) {
     // Create a function that takes a filename as string parameter,
@@ -6,5 +11,26 @@ public class CountAs {
 
     // example: on the input "afile.txt" the function should return 28 - print this result
     // example: on the input "not-a-file" the function should return 0 - print this result
+    int count = countLetterA("afile.txt");
+    System.out.println(count);
+    count = countLetterA("not-a-file.txt");
+    System.out.println(count);
+  }
+
+  private static int countLetterA(String filename) {
+    Path filePath = Paths.get(filename);
+    int count = 0;
+
+    try {
+      for (String line : Files.readAllLines(filePath)) {
+        for (int i = 0; i < line.length(); i++) {
+          if (line.toLowerCase().charAt(i) == 'a') {
+            count++;
+          }
+        }
+      }
+    } catch (IOException e) {
+    }
+    return count;
   }
 }
